03cd2b3f3599cbcec9fc12c3f83e10288a786533
mgladkov <mgladkov@abz-1.ru>
2021-11-26 16:01:11 +0300

    ...

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,11 +7,24 @@
 </template>
 
 <script>
+import moment from 'moment'
 
 export default {
     name: 'App',
+    methods: {
+        switchDayNightMode() {
+            const hour = moment().hour()
+            if(hour >= 18 && hour < 7) {
+                document.body.classList.add('dark-mode')
+            } else {
+                document.body.classList.remove('dark-mode')
+            }
+        }
+    },
     mounted() {
         document.body.classList.add('nk-body', 'npc-invest', 'bg-lighter')
+        this.switchDayNightMode()
+        setInterval(this.switchDayNightMode(), 1000 * 60 * 10)
     }
 }
 </script>
