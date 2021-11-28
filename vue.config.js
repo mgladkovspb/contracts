9fb327436adb7cbb66e9872e6b3019f849840432
Гладков Максим <mgladkov@abz-1.ru>
2021-11-28 13:35:31 +0300

    ...

--- vue.config.js
+++ vue.config.js
@@ -8,9 +8,15 @@ module.exports = {
             .entry("app")
             .clear()
             .add("./frontend/src/main.js")
-            .end();
+            .end()
         config.resolve.alias
             .set("@", path.join(__dirname, "./frontend/src"))
+        config.plugin("html").tap(args => {
+            args[0].inject = true
+            args[0].template = './frontend/public/index.html'
+            args[0].favicon = './frontend/public/favicon.png'
+            return args
+        })
     },
     devServer: {
         /* 
